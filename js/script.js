77459eb837627837ad5a920800be304ea75addf4
veter64 <info@veter64.ru>
2022-04-05 16:21:45 +0400

    Сделал переключение темы сайта

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-"strict";
+"use strict";
+/*Замена классов
+document.querySelector('.page').classList.remove('light-theme-site');
+document.querySelector('.page').classList.add('dark-theme-site');
+*/
+
+/* Переключатель темы */
+let page = document.querySelector('.page');
+let themeButton = document.querySelector('.theme-button');
+
+themeButton.onclick = function() {
+  page.classList.toggle('light-theme-site');
+  page.classList.toggle('dark-theme-site');
+};
