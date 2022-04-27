24d89edb68115b9433b0718581b3e9bf9da5c43e
veter64 <info@veter64.ru>
2022-04-27 23:14:46 +0400

    Сделана галерея

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ document.querySelector('.page').classList.remove('light-theme-site');
 document.querySelector('.page').classList.add('dark-theme-site');
 */
 
+let test = document.querySelector(".button-classic-test");
+test.onclick=function () {
+  alert("Нажали на кнопочку");
+  console.log("Кнопка нажата");
+};
+
 /* Переключатель темы */
 let page = document.querySelector('.page');
 let themeButton = document.querySelector('.theme-button');
@@ -13,7 +19,4 @@ themeButton.onclick = function() {
   page.classList.toggle('light-theme-site');
 };
 
-let test = document.querySelector(".button-classic-test");
-test.onclick=function () {
-  alert("Нажали на кнопочку");
-};
+
